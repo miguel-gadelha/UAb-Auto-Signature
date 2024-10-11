92b2ec38d378db8dbf1dd7622494d508a05353e0
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-10-11 18:05:26 +0100

    chore: move the repo up

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { render, h } from "preact";
 import "./style.css";
 
 import { ConfigPopup } from "./pages/config-popup";
-import { Header } from "./components/header";
 
 export function App() {
   return <ConfigPopup />;
